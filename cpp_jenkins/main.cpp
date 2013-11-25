8cbeaaab6e1e0292cdbc28d06de6412d26fbcb9e
Rainer Schamm <rschamm@rogueware.org>
2013-11-25 11:44:35 +0200

    adding a cpp project

---
+++ cpp_jenkins/main.cpp
@@ -0,0 +1,22 @@
+/* 
+ * File:   main.cpp
+ * Author: rschamm
+ *
+ * Created on November 25, 2013, 11:24 AM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+   cout << "Hello World !!!" << endl;
+   
+   return 0;
+}
+
